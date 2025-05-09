3585f7a4167a4504daa98ba600976533bbdb68ac
garrettrogers002 <garrett.rogers002@gmail.com>
2025-05-09 12:57:17 -0400

    create function to play a round

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch (choice) {
@@ -23,4 +25,28 @@ function getHumanChoice() {
             return "error"
     }
 }
-console.log(getHumanChoice())
+
+function playRound(computerChoice, humanChoice) {
+
+
+    if (humanChoice !== "error") {
+        if (computerChoice === humanChoice) {
+            console.log("tie, go again")
+            playRound();
+        } else if ((computerChoice === "rock" && humanChoice === "scissors") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "scissors" && humanChoice === "paper")) {
+            console.log("computer wins");
+            console.log("computerScore: ", ++computerScore);
+            console.log("humanScore: ", humanScore);
+        } else {
+            console.log("player wins");
+            console.log("computerScore: ", computerScore);
+            console.log("playerScore: ", ++humanScore);
+        }
+    } else {
+        console.log("player choice error");
+    }
+}
+let computer = getComputerChoice();
+let human = getHumanChoice();
+
+playRound(computer, human);
