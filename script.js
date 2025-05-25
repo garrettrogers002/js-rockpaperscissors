5d51fae25aaf501c9346a980a8d3ded34533949e
garrettrogers002 <garrett.rogers002@gmail.com>
2025-05-24 23:32:56 -0400

    add update to result div after each round

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let humanScore = 0, computerScore = 0, winningScore = 5;
 const rockbtn = document.querySelector("#rock");
 const paperbtn = document.querySelector("#paper");
 const scissorsbtn = document.querySelector("#scissors");
+const resultbox = document.querySelector("#result");
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -21,6 +22,11 @@ function getComputerChoice() {
 
 function playGame(human) {
     if (humanScore >= 5 || computerScore >= 5) {
+        if (humanScore >= 5) {
+            resultbox.textContent = `human wins 5 to ${computerScore}`;
+        } else if (computerScore >= 5) {
+            resultbox.textContent = `computer wins 5 to ${humanScore}`;
+        }
         return
     }
     let computerChoice = getComputerChoice();
@@ -28,15 +34,27 @@ function playGame(human) {
 
     if (human !== "error") {
         if (computerChoice === human) {
-            console.log("tie, go again");
+            resultbox.textContent = (`tie, go again, computer score: ${computerScore} humanScore: ${humanScore}`);
         } else if ((computerChoice === "rock" && human === "scissors") || (computerChoice === "paper" && human === "rock") || (computerChoice === "scissors" && human === "paper")) {
-            console.log("computer wins");
-            console.log("computerScore: ", ++computerScore);
-            console.log("humanScore: ", humanScore);
+            resultbox.textContent = (`computer wins, computer score: ${++computerScore}, humanScore: ${humanScore}`);
+            if (humanScore >= 5 || computerScore >= 5) {
+                if (humanScore >= 5) {
+                    resultbox.textContent = `human wins 5 to ${computerScore}`;
+                } else if (computerScore >= 5) {
+                    resultbox.textContent = `computer wins 5 to ${humanScore}`;
+                }
+                return
+            }
         } else {
-            console.log("player wins");
-            console.log("computerScore: ", computerScore);
-            console.log("playerScore: ", ++humanScore);
+            resultbox.textContent = (`player wins, computer score: ${computerScore}, humanScore: ${++humanScore}`)
+            if (humanScore >= 5 || computerScore >= 5) {
+                if (humanScore >= 5) {
+                    resultbox.textContent = `human wins 5 to ${computerScore}`;
+                } else if (computerScore >= 5) {
+                    resultbox.textContent = `computer wins 5 to ${humanScore}`;
+                }
+                return
+            }
         }
     } else {
         console.log("player choice error");
